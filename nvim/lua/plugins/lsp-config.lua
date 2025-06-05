e5ed15c0434d2cc81548b4cac537dee60cd25947
Jeremy Mignolet <jeremy.mignolet@pm.me>
2025-06-05 13:57:16 +0200

    Add none-ls plugin

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,41 +1,41 @@
 return {
-  {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "ansiblels",
-          "terraformls",
-          "tflint",
-          "dockerls",
-          "docker_compose_language_service",
-          "bicep"
-        }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ansiblels.setup({})
-      lspconfig.terraformls.setup({})
-      lspconfig.dockerls.setup({})
-      lspconfig.docker_compose_language_service.setup({})
-      lspconfig.bicep.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ansiblels",
+					"terraformls",
+					"tflint",
+					"dockerls",
+					"docker_compose_language_service",
+					"bicep",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.ansiblels.setup({})
+			lspconfig.terraformls.setup({})
+			lspconfig.dockerls.setup({})
+			lspconfig.docker_compose_language_service.setup({})
+			lspconfig.bicep.setup({})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-      vim.keymap.set({ 'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,15 +1,15 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree toggle float<CR>')
-    vim.keymap.set('n', '<C-b>', ':Neotree toggle left<CR>')
-    vim.keymap.set('n', '<leader>q', ':Neotree action=close<CR>')
-    vim.keymap.set('n', '<leader>g', ':Neotree source=git_status<CR>')
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle float<CR>")
+		vim.keymap.set("n", "<C-b>", ":Neotree toggle left<CR>")
+		vim.keymap.set("n", "<leader>q", ":Neotree action=close<CR>")
+		vim.keymap.set("n", "<leader>g", ":Neotree source=git_status<CR>")
+	end,
 }
