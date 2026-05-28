ecb1097a7a3da6eacdae9cfe7ee814a76e4f4893
Jeremy Mignolet <jeremy@mignolet.io>
2026-05-28 11:48:08 +0200

    add greeting in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,5 +6,10 @@ end
 fish_add_path $HOME/.opencode/bin
 fish_add_path $HOME/.cargo/bin
 
+function fish_greeting
+    echo Hello Jeremy!
+    echo The time is (set_color yellow)(date +%T)(set_color --reset).
+end
+
 starship init fish | source
 zoxide init fish | source
