ca73c8ab465819eca5891c496f932ddf4a1b9bdc
Jeremy Mignolet <jeremy.mignolet@arxus.eu>
2026-05-28 14:04:00 +0200

    Add Bauhaus theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -10,4 +10,14 @@ return {
       vim.cmd("colorscheme ember")
     end,
   },
+  {
+    "somerocketeer/bauhaus.nvim",
+    name = "bauhaus-nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("bauhaus").setup({ transparent = false })
+      vim.cmd.colorscheme("bauhaus")
+    end,
+  },
 }
