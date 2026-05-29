a588a596baf0f2d5c3b2b327659666dd55f2ed0a
Jeremy Mignolet <jeremy.mignolet@arxus.eu>
2026-05-29 15:43:36 +0200

    Add WSL2 config for Zeal

--- .config/nvim/lua/plugins/zeavim.lua
+++ .config/nvim/lua/plugins/zeavim.lua
@@ -2,5 +2,12 @@ return {
   "KabbAmine/zeavim.vim",
   init = function()
     vim.g.zv_file_types = { hcl = "terraform", go = "go" }
+    if vim.fn.has("wsl") == 1 then
+      vim.g.zv_zeal_executable = "/mnt/c/Program Files/Zeal/zeal.exe"
+    elseif vim.fn.has("win32") == 1 then
+      vim.g.zv_zeal_executable = os.getenv("ProgramFiles") .. "\\Zeal\\zeal.exe"
+    else
+      vim.g.zv_zeal_executable = "zeal"
+    end
   end,
 }
