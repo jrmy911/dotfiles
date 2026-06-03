9530674a3e5e3ef222d12f8a106fa57045e9a041
Jeremy Mignolet <jeremy.mignolet@arxus.eu>
2026-06-03 13:10:33 +0200

    Update treesitter to main branch

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,14 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	lazy = false,
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "lua", "vim", "markdown", "terraform" },
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter").setup({
+      ensure_installed = { "lua", "vim", "markdown", "terraform", "go", "gomod", "gosum", "gowork" },
+      auto_install = { enable = true },
+    })
+    -- Highlight and indent are now handled by Neovim's built-in treesitter
+    -- (enabled by default in Neovim 0.10+)
+  end,
 }
