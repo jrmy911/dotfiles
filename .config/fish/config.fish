82f48785cd3f4d226f0a0a6b6af9853b3dd28fc7
Jeremy Mignolet <jeremy@mignolet.io>
2026-08-26 09:34:11 +0200

    Remove opencode & add go path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,9 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
-# opencode
-fish_add_path $HOME/.opencode/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
+fish_add_path $HOME/go/bin/
 
 function fish_greeting
     echo Hello Jeremy!
